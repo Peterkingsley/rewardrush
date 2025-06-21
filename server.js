cac09bd7c40a323863f5cddcfeb2121c49f6a629
Peter Kingsley <peterkingslayer098@gmail.com>
2025-06-21 02:03:00 +0100

    come

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const pool = new Pool({
 });
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'auth.html')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -806,4 +806,4 @@ app.use((req, res, next) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}. Connected to database.`);
 });
-const resetLink
+dirname, 'public
