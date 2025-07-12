6039d3c764fd384138689088edaa51e518dbdcc6
Peter Kingsley <peterkingslayer098@gmail.com>
2025-07-12 18:22:19 +0100

    uijk

--- server.js
+++ server.js
@@ -1896,4 +1896,5 @@ app.use((req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}. Connected to database.`);
+        console.log(`Server running on port ${PORT}. Connected to database.`);
+    });
